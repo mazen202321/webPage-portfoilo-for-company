1eaa09ac7321fb4f1c100a9dbfef905172eef204
mazen202321 <m.s.ahmed@students.ptuk.edu.ps>
2024-03-20 23:51:04 -0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// let navItem=document.getElementsByClassName('nav-item');
+
+// document.getElementById('navbar-toggler').onclick=function(){
+//     for(let x in navItem){
+//         if(navItem[x].style.display=='none'){
+//         navItem[x].style.display = "inline-block";}
+//         else{
+//             navItem[x].style.display = "none";
+//         }
+//     }
+//    };
+// if(document.getElementById('navbar-toggler').style.display=='none'){
+
+// }
